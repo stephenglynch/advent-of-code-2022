ab699315a9c6dc58a5f5508c8722f5d535dddd0d
Stephen Lynch <stepheng.lynch@gmail.com>
2022-12-20 21:32:39 +0000

    day 3

--- day3a/src/main.rs
+++ day3a/src/main.rs
@@ -1,3 +1,30 @@
+use std::fs;
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+static LETTERS: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 fn main() {
-    println!("Hello, world!");
+    let contents = &fs::read_to_string("input/input.txt").unwrap();
+    let pairs: Vec<_> = LETTERS.chars().enumerate().collect();
+
+    // Build priority map
+    let mut priority_map = HashMap::new();
+    for (i, l) in pairs {
+        priority_map.insert(l, i+1);
+    }
+
+    let mut total = 0;
+    for line in contents.lines() {
+        let line_len = line.len() / 2;
+        let left: HashSet<_> = line[..line_len].chars().collect();
+        let right: HashSet<_> = line[line_len..].chars().collect();
+        let shared = left.intersection(&right);
+
+        for l in shared {
+            total += priority_map[l];
+        }
+    }
+    
+    println!("total = {}", total);
 }
--- day3b/src/main.rs
+++ day3b/src/main.rs
@@ -1,3 +1,33 @@
+use std::fs;
+use std::collections::HashMap;
+use std::collections::HashSet;
+use itertools::Itertools;
+
+static LETTERS: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 fn main() {
-    println!("Hello, world!");
+    let contents = &fs::read_to_string("input/input.txt").unwrap();
+    let pairs: Vec<_> = LETTERS.chars().enumerate().collect();
+
+    // Build priority map
+    let mut priority_map = HashMap::new();
+    for (i, l) in pairs {
+        priority_map.insert(l, i+1);
+    }
+
+    let mut total = 0;
+    for (a, b, c) in contents.lines().tuples() {
+        let a: HashSet<_> = a.chars().collect();
+        let b: HashSet<_> = b.chars().collect();
+        let c: HashSet<_> = c.chars().collect();
+
+        let shared: HashSet<_> = a.intersection(&b).cloned().collect();
+        let shared: HashSet<_> = shared.intersection(&c).collect();
+
+        for l in shared {
+            total += priority_map[l];
+        }
+    }
+    
+    println!("total = {}", total);
 }
