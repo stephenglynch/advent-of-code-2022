ddd8140a6c51f133801258def0b3007390b5d555
Stephen Lynch <stepheng.lynch@gmail.com>
2022-12-21 13:40:17 +0000

    day 6

--- day6a/src/main.rs
+++ day6a/src/main.rs
@@ -1,3 +1,19 @@
+use std::{fs, collections::HashSet};
+
+fn find_start(s: &str) -> usize {
+
+    let v: Vec<char> = s.chars().collect();
+    let windows = v.windows(4);
+    for (i, w) in windows.enumerate() {
+        let set: HashSet<char> = w.iter().cloned().collect();
+        if set.len() == 4 {
+            return i + 4;
+        }
+    }
+    return 0;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("input/input.txt").unwrap();
+    println!("answer = {}", find_start(&contents));
 }
--- day6b/src/main.rs
+++ day6b/src/main.rs
@@ -1,3 +1,21 @@
+use std::{fs, collections::HashSet};
+
+const UNIQUE_SEQ: usize = 14;
+
+fn find_start(s: &str) -> usize {
+
+    let v: Vec<char> = s.chars().collect();
+    let windows = v.windows(UNIQUE_SEQ);
+    for (i, w) in windows.enumerate() {
+        let set: HashSet<char> = w.iter().cloned().collect();
+        if set.len() == UNIQUE_SEQ {
+            return i + UNIQUE_SEQ;
+        }
+    }
+    return 0;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("input/input.txt").unwrap();
+    println!("answer = {}", find_start(&contents));
 }
