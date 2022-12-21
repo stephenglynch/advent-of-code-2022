6752b8a8fd71b89b9c1d50b018d784a74df214bc
Stephen Lynch <stepheng.lynch@gmail.com>
2022-12-21 13:23:48 +0000

    day 4

--- day4b/src/main.rs
+++ day4b/src/main.rs
@@ -1,3 +1,33 @@
+use std::fs;
+use std::collections::HashSet;
+use regex::Regex;
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref PATTERN: Regex = Regex::new(r"^(\d+)-(\d+),(\d+)-(\d+)$").unwrap();
+}
+
+fn parse_line(line: &str) -> (HashSet<u32>, HashSet<u32>) {
+    let caps = PATTERN.captures(line).unwrap();
+    let s1: u32 = caps[1].parse().unwrap();
+    let e1: u32 = caps[2].parse().unwrap();
+    let s2: u32 = caps[3].parse().unwrap();
+    let e2: u32 = caps[4].parse().unwrap();
+
+    ((s1..=e1).collect(), (s2..=e2).collect())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let contents = &fs::read_to_string("input/input.txt").unwrap();    
+
+    let mut total = 0;
+    for l in contents.lines() {
+        let (a, b) = parse_line(l);
+
+        if !a.is_disjoint(&b) {
+            total += 1;
+        }
+    }
+
+    println!("answer = {}", total);
 }
